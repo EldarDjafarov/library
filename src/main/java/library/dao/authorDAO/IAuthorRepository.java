7002c5d3c74bd7954e26ae4de778ab1bc7f30291
ResaterJke <ildardjafarov2@gmail.com>
2019-09-24 11:13:40 +0300

    add security and swagger

--- src/main/java/library/dao/authorDAO/IAuthorRepository.java
+++ src/main/java/library/dao/authorDAO/IAuthorRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IAuthorRepository extends JpaRepository<Author,Long> {
-    Author findByName(String name);
+    Author findByName(String fullName);
 
 
 
